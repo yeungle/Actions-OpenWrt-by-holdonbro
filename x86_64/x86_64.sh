5296c43538772be6ac481e83f25458add10c9642
yeungle <75871729+yeungle@users.noreply.github.com>
2024-03-22 20:30:25 +0800

    Update x86_64.sh

--- x86_64/x86_64.sh
+++ x86_64/x86_64.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 #sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
